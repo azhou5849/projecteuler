c7c0a87755fb52f4f1149c2c60ae4395c563743a
azhou5849 <67875175+azhou5849@users.noreply.github.com>
2022-04-16 16:56:59 -0400

    Update p0005.py

--- p0005.py
+++ p0005.py
@@ -1,1 +1,20 @@
 import math
+
+def gcd(a,b):
+    '''Takes in two positive integers and returns the gcd computed by the Euclidean algorithm'''
+    if a > b:
+        return gcd(b,a)
+    elif a == 0:
+        return b
+    else:
+        return gcd(b % a, a)
+
+def lcm(a,b):
+    '''Takes in two positive integers and returns the lcm by the relation ab = gcd(a,b) * lcm(a,b)'''
+    return a * b / gcd(a,b)
+
+mult = 1
+for n in range(2,21):
+    mult = lcm(mult, n)
+
+print(mult)
