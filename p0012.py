1b49ccdbdeed7d0499fe06628a1a4f19baf4aa38
azhou5849 <67875175+azhou5849@users.noreply.github.com>
2022-04-20 22:46:32 -0400

    Update p0012.py

--- p0012.py
+++ p0012.py
@@ -8,4 +8,10 @@ def num_divisors(n):
         count = count + (2 if n % d == 0 else 0)
     return count + (r * r == n)
 
-num_divisors(140)
+d, n = 0, 0
+while d <= 500:
+    # we'll use the fact that d(n) is multiplicative
+    n = n + 1
+    d = num_divisors(int(n / 2)) * num_divisors(n + 1) if n % 2 == 0 else num_divisors(n) * num_divisors(int((n + 1) / 2))
+
+print(int(n * (n + 1) / 2))
