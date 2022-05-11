341c946f9d29da513e9b0d8e47a40e2531f3a519
azhou5849 <67875175+azhou5849@users.noreply.github.com>
2022-05-11 16:48:43 -0400

    Update p0018.py

--- p0018.py
+++ p0018.py
@@ -7,4 +7,13 @@ for line in f:
     triangle.append([int(n) for n in line.split()])
 f.close()
 
-triangle
+maximums = [[0 for n in row] for row in triangle]
+
+for i in range(-1, -len(maximums) - 1, -1):
+    if i == -1:
+        maximums[i] = triangle[i]
+    else:
+        for j in range(len(maximums[i])):
+            maximums[i][j] = max(maximums[i + 1][j], maximums[i + 1][j + 1]) + triangle[i][j]
+
+maximums[0][0]
