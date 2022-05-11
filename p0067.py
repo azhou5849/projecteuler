03964c185e92ea3d3aafc53636a7b704519a5e12
azhou5849 <67875175+azhou5849@users.noreply.github.com>
2022-05-11 16:51:43 -0400

    Problem 67

---
+++ p0067.py
@@ -0,0 +1,19 @@
+import math
+
+triangle = []
+
+f = open('p0067triangle.txt')
+for line in f:
+    triangle.append([int(n) for n in line.split()])
+f.close()
+
+maximums = [[0 for n in row] for row in triangle]
+
+for i in range(-1, -len(maximums) - 1, -1):
+    if i == -1:
+        maximums[i] = triangle[i]
+    else:
+        for j in range(len(maximums[i])):
+            maximums[i][j] = max(maximums[i + 1][j], maximums[i + 1][j + 1]) + triangle[i][j]
+
+maximums[0][0]
