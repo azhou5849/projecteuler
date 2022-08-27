c9c8a6868703dc8d87ee5de8668db3e0ab649dcd
azhou5849 <67875175+azhou5849@users.noreply.github.com>
2022-08-27 12:28:59 -0400

    Create p0042.py

---
+++ p0042.py
@@ -0,0 +1,28 @@
+"""
+The nth term of the sequence of triangle numbers is given by, tn = n(n+1)/2; so the first ten triangle numbers are:
+1, 3, 6, 10, 15, 21, 28, 36, 45, 55, ...
+By converting each letter in a word to a number corresponding to its alphabetical position and adding these values we form a word value. For example, the word value for SKY is 19 + 11 + 25 = 55 = t10. If the word value is a triangle number then we shall call the word a triangle word.
+Using words.txt (right click and 'Save Link/Target As...'), a 16K text file containing nearly two-thousand common English words, how many are triangle words?
+"""
+def word_val(w):
+    """
+    Computes the word value for the word w, assumed in all caps
+    """
+    return sum([ord(c) - 64 for c in w])
+
+with open("p0042words.txt", 'r') as f:
+    words = [w.strip('"') for w in f.readline().strip().split(',')]
+values = [word_val(w) for w in words]
+max_value = max(values)
+
+is_triangle = [False for n in range(max_value + 1)]
+t,i = 0,1
+while t <= max_value:
+    is_triangle[t] = True
+    t += i
+    i += 1
+
+count = 0
+for v in values:
+    count += is_triangle[v]
+print(count)
