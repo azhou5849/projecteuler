697ccb3cd93e937a0b674232e648cd41395b16bc
azhou5849 <67875175+azhou5849@users.noreply.github.com>
2022-08-28 22:54:33 -0400

    Update p0054.py

--- p0054.py
+++ p0054.py
@@ -19,3 +19,79 @@ Each line of the file contains ten cards (separated by a single space): the firs
 You can assume that all hands are valid (no invalid characters or repeated cards), each player's hand is in no specific order, and in each hand there is a clear winner.
 How many hands does Player 1 win?
 """
+rank_chars = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'T': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14}
+
+class Card:
+    """
+    Ranks are stored as ints from 2 through 14 (J = 11, Q = 12, K = 13, A = 14)
+    Suits are stored as characters from ['C', 'D', 'H', 'S']
+    Cards are totally ordered, with suits being ordered alphabetically (but Hands will not be)
+    """
+    def __init__(self, card_str):  # card_str is a string of two characters, first rank then suit
+        self.rank = rank_chars[card_str[0]]
+        self.suit = card_str[1]
+
+    def __eq__(self, other):
+        return self.rank == other.rank and self.suit == other.suit
+
+    def __lt__(self, other):
+        if self.rank < other.rank:
+            return True
+        elif self.rank == other.rank:
+            return self.suit < other.suit
+        else:
+            return False
+
+    def __le__(self, other):
+        if self.rank < other.rank:
+            return True
+        elif self.rank == other.rank:
+            return self.suit <= other.suit
+        else:
+            return False
+
+
+class Hand:
+    def __init__(self, card_list = []):
+        self.card_list = card_list
+
+    def add_card(self, card):
+        self.card_list.append(card)
+        self.card_list.sort(reverse = True)
+
+    def evaluate(self):
+        if len(self.card_list) != 5:
+            raise Exception("Something went wrong")
+        unique_suits = []
+        for c in self.card_list:
+            if c.suit not in unique_suits:
+                unique_suits.append(c.suit)
+        if len(unique_suits) == 1:
+            ranks = [c.rank for c in self.card_list]  # this should already be sorted
+            if ranks[4] - ranks[0] == 4:
+                if ranks[0] == 10:
+                    self.type = "Royal Flush"
+                else:
+                    self.type = "Straight Flush"
+            else:
+                self.type = "Flush"
+        else:
+            
+
+    @classmethod
+    def read_hands(cls, line):
+        """
+        Returns a Hand pair read from a line of the file
+        """
+        card_strs = line.split()
+        first_hand, second_hand = Hand([]), Hand([])
+        for i in range(10):
+            if i < 5:
+                first_hand.add_card(Card(card_strs[i]))
+            else:
+                second_hand.add_card(Card(card_strs[i]))
+        return first_hand, second_hand
+
+with open("p0054poker.txt", 'r') as f:
+    line = f.readline()
+    first_hand, second_hand = Hand.read_hands(line)
