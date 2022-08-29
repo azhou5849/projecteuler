886bbab7ae82e9cc87a112f09e88099d61fd7b8a
azhou5849 <67875175+azhou5849@users.noreply.github.com>
2022-08-28 23:32:07 -0400

    Update p0054.py

--- p0054.py
+++ p0054.py
@@ -20,6 +20,16 @@ You can assume that all hands are valid (no invalid characters or repeated cards
 How many hands does Player 1 win?
 """
 rank_chars = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'T': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14}
+type_order = [  "High Card",
+                "One Pair",
+                "Two Pairs",
+                "Three of a Kind",
+                "Straight",
+                "Flush",
+                "Full House",
+                "Four of a Kind",
+                "Straight Flush",
+                "Royal Flush"  ]
 
 class Card:
     """
@@ -57,26 +67,40 @@ class Hand:
 
     def add_card(self, card):
         self.card_list.append(card)
-        self.card_list.sort(reverse = True)
+        self.card_list.sort()
 
     def evaluate(self):
         if len(self.card_list) != 5:
             raise Exception("Something went wrong")
-        unique_suits = []
+        is_flush = True
         for c in self.card_list:
-            if c.suit not in unique_suits:
-                unique_suits.append(c.suit)
-        if len(unique_suits) == 1:
+            if c.suit != c[0].suit:
+                is_flush = False
+        if is_flush:
             ranks = [c.rank for c in self.card_list]  # this should already be sorted
             if ranks[4] - ranks[0] == 4:
                 if ranks[0] == 10:
                     self.type = "Royal Flush"
                 else:
                     self.type = "Straight Flush"
+                    self.tiebreak = ranks[0]
             else:
                 self.type = "Flush"
+                self.tiebreak = ranks
         else:
-            
+            unique_ranks = []
+            for c in self.card_list:
+                if c.rank not in unique_ranks:
+                    unique_ranks.append(c.rank)  # this should already be sorted from top to bottom
+            if len(unique_ranks) == 2:  # split is either 4-1 or 3-2
+                if unique_ranks[0] < unique_ranks[1] or unique_ranks[3] < unique_ranks[4]:
+                    self.type = "Four of a Kind"
+                else:
+                    self.type = "Full House"
+            elif len(unique_ranks) == 3:
+                if
+
+
 
     @classmethod
     def read_hands(cls, line):
