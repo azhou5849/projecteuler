5a5bed1938564f337df47013d0a69cd6729ffa07
azhou5849 <67875175+azhou5849@users.noreply.github.com>
2022-09-03 01:34:42 -0400

    Update p0060.py
    solved -- need to optimise

--- p0060.py
+++ p0060.py
@@ -2,24 +2,32 @@
 The primes 3, 7, 109, and 673, are quite remarkable. By taking any two primes and concatenating them in any order the result will always be prime. For example, taking 7 and 109, both 7109 and 1097 are prime. The sum of these four primes, 792, represents the lowest sum for a set of four primes with this property.
 Find the lowest sum for a set of five primes for which any two primes concatenate to produce another prime.
 -----------------------------------------
-We build a graph where each node is a prime number and we draw an edge p -> q if the concatenation pq is prime
-We are looking for a complete directed graph on 5 nodes
+We build a graph where each node is a prime number and we draw an edge p -- q if the concatenations pq and qp are prime
+We are looking for a K5 subgraph
 """
-import numpy as np
-
-def count_clique(adj, k):
+def list_cliques(n, edges, k):
     """
-    Given a graph specified by the adjacency matrix adj, and a positive integer k,
-    count the number of k-cliques in the graph.
+    Given a graph specified by the number of vertices n and dictionary 'edges' with keys 0, 1, ..., n - 1
+    and sets edges[i] with j in edges[i] if and only if i and j are connected,
+    and a positive integer k, return a list of all k-cliques in the graph.
     """
-    rows, cols = adj.shape
-    if k == 3:
-        square = np.matmul(adj, adj)
-        return np.sum(np.multiply(adj, square)) // 6
-    else:
-        return "potato"
+    if k == 1:
+        return [[v] for v in range(n)]
+    smaller_cliques = list_cliques(n, edges, k - 1)
+    output = []
+    for C in smaller_cliques:
+        start = max(C) + 1  # this ensures that each clique is listed exactly once with vertices in increasing order
+        for i in range(start, n):
+            forms_clique = True
+            for v in C:
+                if v not in edges[i]:
+                    forms_clique = False
+                    break
+            if forms_clique:
+                output.append(C + [i])
+    return output
 
-d = 3  # number of digits to work up to for nodes (so we need primes up to 2d digits), adjust as needed
+d = 4  # number of digits to work up to for nodes (so we need primes up to 2d digits), adjust as needed
 is_prime = [True for _ in range(10 ** (2 * d))]
 is_prime[0] = False
 is_prime[1] = False
@@ -31,13 +39,17 @@ for n in range(2, 10 ** (2 * d)):
             m += n
 primes = [p for p in range(10 ** d) if is_prime[p]]
 
-adj = np.zeros((len(primes), len(primes)), dtype = int)
+edges = {i: set() for i in range(len(primes))}
 for i in range(len(primes) - 1):
     for j in range(i + 1, len(primes)):
         n = int(str(primes[i]) + str(primes[j]))
         m = int(str(primes[j]) + str(primes[i]))
         if is_prime[n] and is_prime[m]:
-            adj[i,j] = 1
-            adj[j,i] = 1
+            edges[i].add(j)
+            edges[j].add(i)
 
-count_clique(adj, 3)
+indices = list_cliques(len(primes), edges, 5)
+print(indices)
+
+for I in indices:
+    print(sum([primes[i] for i in I]))
